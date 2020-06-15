29f0a3720b4d45007c9f012784b4648f7656b9ee
b-ed <67429249+b-ed@users.noreply.github.com>
2020-06-15 23:15:08 +0300

    Updates

---
+++ precache-manifest.410157b40a278bd9eff1a69d54663014.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702067324501f10caf44567d4117dcff",
+    "url": "/LeitnerSeven/index.html"
+  },
+  {
+    "revision": "1ca8b84c28f6ee2f3185",
+    "url": "/LeitnerSeven/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3ffd00fdbac4841e2ae2",
+    "url": "/LeitnerSeven/static/css/main.149c1e23.chunk.css"
+  },
+  {
+    "revision": "1ca8b84c28f6ee2f3185",
+    "url": "/LeitnerSeven/static/js/2.216f36a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LeitnerSeven/static/js/2.216f36a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ffd00fdbac4841e2ae2",
+    "url": "/LeitnerSeven/static/js/main.b3224174.chunk.js"
+  },
+  {
+    "revision": "53f1f06e209083e443b4",
+    "url": "/LeitnerSeven/static/js/runtime-main.c5fda03a.js"
+  },
+  {
+    "revision": "84d8b93ea0097a78e5101ac433b36148",
+    "url": "/LeitnerSeven/static/media/logo.84d8b93e.png"
+  }
+]);
